47ae8c5969b94de5d4ddc3698fb23aeddc85f030
hermes26 <hermitte.thierry@gmail.com>
2019-09-17 17:13:15 +0200

    added html bootstrap content to all the views

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req,res) => res.send('Login'));
+router.get('/login', (req,res) => res.render('login.ejs'));
 
-router.get('/register', (req,res) => res.send('Register'));
+router.get('/register', (req,res) => res.render('register.ejs'));
 
 
 
