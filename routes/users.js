1fcff04467c32a7c1164b13438fabaf0dcc715d5
hermes26 <hermitte.thierry@gmail.com>
2019-09-18 11:31:15 +0200

    added route for register post request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+//LOGIN page
 router.get('/login', (req,res) => res.render('login.ejs'));
 
+//REGISTER page
 router.get('/register', (req,res) => res.render('register.ejs'));
+
+//Register Handle submit/post request
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body; //what we pass in the form will be in the req.body
+    
+    res.send('hello');
+});
 
 
 
