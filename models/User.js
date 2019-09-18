0861d6ade7e1ebf5caf3f6d70727d6e22f5920b6
hermes26 <hermitte.thierry@gmail.com>
2019-09-18 11:17:46 +0200

    created schema using mongoose

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+//create a model from our schema
+const User = mongoose.model('User', 'UserSchema'); //model name: User; model schema: userSchema
+
+module.exports = User;
